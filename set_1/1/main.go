8eed0d3d8d4ecd14c5e0ea5c4dda6e5bd523e7d0
Jack Rieck <jack.rieck@sendgrid.com>
2020-05-12 21:26:30 -0600

    got it

--- set_1/1/main.go
+++ set_1/1/main.go
@@ -8,8 +8,10 @@ import (
 func main() {
 
 	hexStr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
 	b := ASCIIToHex(hexStr)
 	fmt.Println(string(b))
+
 	x := ByteStringToBase64(b)
 	fmt.Println(x)
 }
